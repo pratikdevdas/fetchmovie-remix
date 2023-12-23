45b5d888e25c86493a8704342bf8a961e9027ee1
Revenant <dasdev.pratik@gmail.com>
2023-12-24 04:48:14 +0530

    UI under construction banner

--- app/root.tsx
+++ app/root.tsx
@@ -18,28 +18,28 @@ export const links: LinksFunction = () => {
   return [
     ...(cssBundleHref
       ? [
-        { rel: 'stylesheet', href: cssBundleHref },
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.googleapis.com'
-        },
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.gstatic.com"'
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@700;800&display=swap'
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600&display=swap'
-        },
-        {
-          rel: 'stylesheet',
-          href: main
-        }
-      ]
+          { rel: 'stylesheet', href: cssBundleHref },
+          {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com'
+          },
+          {
+            rel: 'preconnect',
+            href: 'https://fonts.gstatic.com"'
+          },
+          {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@700;800&display=swap'
+          },
+          {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600&display=swap'
+          },
+          {
+            rel: 'stylesheet',
+            href: main
+          }
+        ]
       : [])
   ]
 }
@@ -53,20 +53,23 @@ export const Head = createHead(() => (
 ))
 
 export default function App() {
-
   const [wishlistId, setWishlistId] = useState('')
   const [secretWishlistId, setSecretWishlistId] = useState('')
-  const [,setSearchParams] = useSearchParams('')
+  const [, setSearchParams] = useSearchParams('')
 
   useEffect(() => {
     const existingSession = window.localStorage.getItem('localUserWishlistData')
     if (existingSession) {
-      const existingSessionJSON = JSON.parse(existingSession ? existingSession : '')
+      const existingSessionJSON = JSON.parse(
+        existingSession ? existingSession : ''
+      )
       setWishlistId(existingSessionJSON.wishlistId)
       setSecretWishlistId(existingSessionJSON.secretId)
-      setSearchParams({ wl: existingSessionJSON.wishlistId, sid: existingSessionJSON.secretId })
+      setSearchParams({
+        wl: existingSessionJSON.wishlistId,
+        sid: existingSessionJSON.secretId
+      })
     } else {
-
       const wishlistId = v4()
       const secretId = v1()
       console.log(wishlistId)
@@ -82,6 +85,8 @@ export default function App() {
   return (
     <>
       <Head />
+      <p style={{ textAlign:'center', font: 'normal', color:'#fff', marginTop : '8px' }}>⚠️ New UI is under construction. You might loose your wishlist data! <a style={{ textDecoration: 'underline', color:'#e3242b' }} href="mailto:pratikdevdas@outlook.com">Mail me</a> with your wishlist url to get it now</p>
+
       <Outlet context={{ wishlistId, secretWishlistId }} />
       {/* removed because scroll was going up on loading modal */}
       {/* <ScrollRestoration /> */}
